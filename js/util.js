a889eb44099b29bebc56969ec7652c54b319afa1
Yoshiya Hinosawa <stibium121@gmail.com>
2014-11-01 17:13:56 +0000

    Add wanderer-leave-grid feature

--- js/util.js
+++ js/util.js
@@ -3,19 +3,24 @@ var dice = function (n) {
     return Math.floor(Math.random() * n);
 };
 
-var bindEvents = function (up, down, left, right) {
+var bindEvents = function (up, down, left, right, command) {
 
-    $('.wrapper').swipeCross()
-    .on('swipeup', function () { console.log('swipeup'); up();})
-    .on('swiperight', function () { console.log('swiperight'); right(); })
-    .on('swipeleft', function () { console.log('swipeleft'); left(); })
-    .on('swipedown', function () { console.log('swipedown'); down(); })
-    .on('swipecancel', function () { console.log('swipecancel'); });
+    $('.wrapper')
+        .swipeCross()
+        .on('swipeup', function () { console.log('swipeup'); up();})
+        .on('swiperight', function () { console.log('swiperight'); right(); })
+        .on('swipeleft', function () { console.log('swipeleft'); left(); })
+        .on('swipedown', function () { console.log('swipedown'); down(); })
+        .on('swipecancel', function () { console.log('swipecancel'); });
 
-    $(document).arrowkeys()
-    .on('upkey', function () { console.log('upkey'); up();})
-    .on('rightkey', function () { console.log('rightkey'); right(); })
-    .on('leftkey', function () { console.log('leftkey'); left(); })
-    .on('downkey', function () { console.log('downkey'); down(); })
+    $(document)
+        .arrowkeys()
+        .on('upkey', function () { console.log('upkey'); up();})
+        .on('rightkey', function () { console.log('rightkey'); right(); })
+        .on('leftkey', function () { console.log('leftkey'); left(); })
+        .on('downkey', function () { console.log('downkey'); down(); });
+
+    $(document)
+        .on('command', function () { console.log('command'); command(e); })
 
 };
